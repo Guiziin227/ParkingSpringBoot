ac64385dfa5808035f686d7d73fed8fb275cb825
Guiziin227 <gh63388@gmail.com>
2025-04-30 17:26:25 -0300

    fix: change UserRepository ID type from Integer to Long

--- src/main/java/com/guiweber/estacionamento/repository/UserRepository.java
+++ src/main/java/com/guiweber/estacionamento/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.guiweber.estacionamento.repository;
 import com.guiweber.estacionamento.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
