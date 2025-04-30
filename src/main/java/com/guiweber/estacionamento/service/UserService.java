6df160028ab423c8dcdef8baacfb000340902720
Guiziin227 <gh63388@gmail.com>
2025-04-30 17:37:31 -0300

    feat: implement user creation and retrieval in UserController and UserService

--- src/main/java/com/guiweber/estacionamento/service/UserService.java
+++ src/main/java/com/guiweber/estacionamento/service/UserService.java
@@ -1,8 +1,12 @@
 package com.guiweber.estacionamento.service;
 
+import com.guiweber.estacionamento.entities.User;
 import com.guiweber.estacionamento.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +14,13 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
--- src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
+++ src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
@@ -1,9 +1,20 @@
 package com.guiweber.estacionamento.web.controller;
 
+import com.guiweber.estacionamento.entities.User;
 import com.guiweber.estacionamento.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
@@ -11,5 +22,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User u = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(u);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User u = userService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(u);
+    }
 
 }
