2530e6547138a2f94415556b7158d9860bf28a4c
Guiziin227 <gh63388@gmail.com>
2025-04-30 17:47:41 -0300

    feat: add password update and user retrieval functionality in UserController and UserService

--- src/main/java/com/guiweber/estacionamento/service/UserService.java
+++ src/main/java/com/guiweber/estacionamento/service/UserService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,5 +23,17 @@ public class UserService {
     @Transactional(readOnly = true)
     public User findById(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    @Transactional
+    public User editPassword(Long id, String newPassword) {
+        User u = findById(id);
+        u.setPassword(newPassword);
+        return u;
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 }
--- src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
+++ src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -34,6 +36,18 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         User u = userService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(u);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
+        User u = userService.editPassword(id, user.getPassword());
+        return ResponseEntity.status(HttpStatus.OK).body(u);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
 }
