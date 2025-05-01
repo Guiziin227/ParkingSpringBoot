5c6903952c1e9bb348dbb713c30c67eae012c439
Guiziin227 <gh63388@gmail.com>
2025-05-01 18:03:15 -0300

    feat: enhance UserController with OpenAPI annotations for improved API documentation and user management endpoints

--- src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
+++ src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
@@ -6,6 +6,12 @@ import com.guiweber.estacionamento.web.dto.UserCreateDto;
 import com.guiweber.estacionamento.web.dto.UserPasswordDto;
 import com.guiweber.estacionamento.web.dto.UserResponseDto;
 import com.guiweber.estacionamento.web.dto.mapper.UserMapper;
+import com.guiweber.estacionamento.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "User", description = "Endpoints for user management")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
@@ -29,6 +36,20 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "Create a new user",responses = {
+            @ApiResponse(responseCode = "201", description = "User created successfully", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDto.class)
+            )),
+            @ApiResponse(responseCode = "409", description = "User already exists", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            )),
+            @ApiResponse(responseCode = "422", description = "Invalid request data", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            ))
+    })
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
         User u = userService.save(UserMapper.toUser(userCreateDto));
@@ -36,18 +57,48 @@ public class UserController {
     }
 
 
+    @Operation(summary = "Get user by id ",responses = {
+           @ApiResponse(responseCode = "200", description = "User found", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDto.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            ))
+    })
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
         User u = userService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(UserMapper.toDto(u));
     }
 
+    @Operation(summary = "Update user password",responses = {
+            @ApiResponse(responseCode = "204", description = "Password updated successfully", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDto.class)
+            )),
+            @ApiResponse(responseCode = "400", description = "Current password not match", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "Resource not found", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            ))
+    })
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id,@Valid @RequestBody UserPasswordDto user) {
         User u = userService.editPassword(id, user.getPassword(),user.getNewPassword(),user.getConfirmPassword());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @Operation(summary = "Get all users",responses = {
+            @ApiResponse(responseCode = "200", description = "Users found", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDto.class)
+            ))
+    })
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         List<User> users = userService.findAll();
--- src/main/java/com/guiweber/estacionamento/web/dto/UserResponseDto.java
+++ src/main/java/com/guiweber/estacionamento/web/dto/UserResponseDto.java
@@ -1,5 +1,6 @@
 package com.guiweber.estacionamento.web.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.ToString;
 @ToString
 public class UserResponseDto {
 
+    @Schema(example = "1")
     private Long id;
     private String username;
     private String role;
