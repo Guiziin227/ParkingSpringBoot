817a8829e34279b3419389bdec92fbf1ceef6a22
Guiziin227 <gh63388@gmail.com>
2025-05-02 15:55:13 -0300

    feat: add integration test for user creation handling username conflicts

--- src/test/java/com/guiweber/estacionamento/UserIT.java
+++ src/test/java/com/guiweber/estacionamento/UserIT.java
@@ -114,5 +114,19 @@ public class UserIT {
     }
 
 
+    @Test
+    public void createUser_WithUsernameConflicts_ReturnUserCreatedWith409() {
+        ErrorMessage responseBody = testClient.post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserCreateDto("malu@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+    }
 
 }
