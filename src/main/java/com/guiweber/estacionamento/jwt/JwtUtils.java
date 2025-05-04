ea8003e0345d4afe673fc10e391b603fab58051a
Guiziin227 <gh63388@gmail.com>
2025-05-04 18:39:57 -0300

    feat: add JwtUtils class for JWT token creation and validation

---
+++ src/main/java/com/guiweber/estacionamento/jwt/JwtUtils.java
@@ -0,0 +1,95 @@
+package com.guiweber.estacionamento.jwt;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
+public class JwtUtils {
+
+    public static final String JWT_BEARER = "Bearer ";
+    public static final String JWT_AUTHORIZATION = "Authorization";
+    public static final String JWT_SECRET = "0123456789-0123456789-0123456789";
+    public static final long JWT_EXPIRE_DAYS = 0;
+    public static final long JWT_EXPIRE_HOURS = 0;
+    public static final long JWT_EXPIRE_MINUTES = 2;
+
+    private JwtUtils(){}
+
+    private static SecretKey generateKey(){
+        return Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static Date toExpireDate(Date start){
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime expireDate = dateTime.plusDays(JWT_EXPIRE_DAYS).plusHours(JWT_EXPIRE_HOURS).plusMinutes(JWT_EXPIRE_MINUTES);
+        return Date.from(expireDate.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static JwtToken createToken(String username, String role) {
+        Date issuedAt = new Date();
+        Date limit = toExpireDate(issuedAt);
+        String token = Jwts.builder()
+                .header().add("typ", "JWT")
+                .and()
+                .subject(username)
+                .issuedAt(issuedAt)
+                .expiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .claim("role", role)
+                .compact();
+        return new JwtToken(token);
+    }
+
+    private static Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(generateKey())
+                    .build()
+                    .parseSignedClaims(refactorToken(token)).getPayload();
+        } catch (JwtException ex) {
+            log.error(String.format("Token invalido %s", ex.getMessage()));
+        }
+        return null;
+    }
+
+    public static String getUsernameFromToken(String token) {
+        Claims claims = getClaimsFromToken(token);
+        if (claims != null) {
+            return claims.getSubject();
+        }
+        return null;
+    }
+
+    public static boolean isTokenValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(generateKey())
+                    .build()
+                    .parseSignedClaims(refactorToken(token));
+            return true;
+        } catch (JwtException ex) {
+            log.error(String.format("Token invalido %s", ex.getMessage()));
+        }
+        return false;
+    }
+
+    private static String refactorToken(String token) {
+        if (token.contains(JWT_BEARER)) {
+            return token.substring(JWT_BEARER.length());
+        }
+        return token;
+    }
+
+
+}
