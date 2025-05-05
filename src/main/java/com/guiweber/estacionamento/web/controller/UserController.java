3ac3e54b1385d1176c8607fdca9e2c6435367f16
Guiziin227 <gh63388@gmail.com>
2025-05-05 17:47:35 -0300

    refactor: rename User class to Usuario in UserController and UserMapper, update related method references

--- src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
+++ src/main/java/com/guiweber/estacionamento/web/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.guiweber.estacionamento.web.controller;
 
-import com.guiweber.estacionamento.entities.User;
+import com.guiweber.estacionamento.entities.Usuario;
 import com.guiweber.estacionamento.service.UserService;
 import com.guiweber.estacionamento.web.dto.UserCreateDto;
 import com.guiweber.estacionamento.web.dto.UserPasswordDto;
@@ -16,7 +16,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -52,7 +51,7 @@ public class UserController {
     })
     @PostMapping
     public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserCreateDto userCreateDto) {
-        User u = userService.save(UserMapper.toUser(userCreateDto));
+        Usuario u = userService.save(UserMapper.toUser(userCreateDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toDto(u));
     }
 
@@ -69,7 +68,7 @@ public class UserController {
     })
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserResponseDto> getUserById(@PathVariable Long id) {
-        User u = userService.findById(id);
+        Usuario u = userService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(UserMapper.toDto(u));
     }
 
@@ -89,7 +88,7 @@ public class UserController {
     })
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id,@Valid @RequestBody UserPasswordDto user) {
-        User u = userService.editPassword(id, user.getPassword(),user.getNewPassword(),user.getConfirmPassword());
+        Usuario u = userService.editPassword(id, user.getPassword(),user.getNewPassword(),user.getConfirmPassword());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
@@ -101,8 +100,8 @@ public class UserController {
     })
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
-        List<User> users = userService.findAll();
-        return ResponseEntity.status(HttpStatus.OK).body(UserMapper.toUsers(users));
+        List<Usuario> users = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(UserMapper.toListDto(users));
     }
 
 }
--- src/main/java/com/guiweber/estacionamento/web/dto/mapper/UserMapper.java
+++ src/main/java/com/guiweber/estacionamento/web/dto/mapper/UserMapper.java
@@ -1,44 +1,38 @@
 package com.guiweber.estacionamento.web.dto.mapper;
 
-import com.guiweber.estacionamento.entities.User;
-import com.guiweber.estacionamento.entities.enums.Role;
+import com.guiweber.estacionamento.entities.Usuario;
 import com.guiweber.estacionamento.web.dto.UserCreateDto;
 import com.guiweber.estacionamento.web.dto.UserResponseDto;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserMapper {
 
-    public static User toUser(UserCreateDto userCreateDto) {
-        ModelMapper modelMapper = new ModelMapper();
-        return modelMapper.map(userCreateDto, User.class);
+
+    public static Usuario toUser(UserCreateDto createDto) {
+        return new ModelMapper().map(createDto, Usuario.class);
     }
 
-    public static UserResponseDto toDto(User user) {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
-        // Mapear os campos básicos (id, username)
-        UserResponseDto dto = modelMapper.map(user, UserResponseDto.class);
-
-        // Converter o role manualmente
-        String role = user.getRole().toString().replace("ROLE_", "");
-        dto.setRole(role);
-
-        return dto;
+    public static UserResponseDto toDto(Usuario usuario) {
+        String role = usuario.getRole().name().substring("ROLE_".length());
+        PropertyMap<Usuario, UserResponseDto> props = new PropertyMap<Usuario, UserResponseDto>() {
+            @Override
+            protected void configure() {
+                map().setRole(role);
+            }
+        };
+        ModelMapper mapper = new ModelMapper();
+        mapper.addMappings(props);
+        return mapper.map(usuario, UserResponseDto.class);
     }
 
-    public static List<UserResponseDto> toUsers(List<User> users) {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-
-        List<UserResponseDto> dtos = new ArrayList<>();
-        for (User user : users) {
-            dtos.add(toDto(user));
-        }
-        return dtos;
+    public static List<UserResponseDto> toListDto(List<Usuario> usuarios) {
+        return usuarios.stream().map(user -> toDto(user)).collect(Collectors.toList());
     }
+
 }
