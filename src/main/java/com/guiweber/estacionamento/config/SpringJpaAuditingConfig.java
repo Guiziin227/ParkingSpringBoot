c34937a78707d5aee2e998116019ef7f9de50bb3
Guiziin227 <gh63388@gmail.com>
2025-05-05 18:09:35 -0300

    feat: enable JPA auditing by adding @EnableJpaAuditing annotation

--- src/main/java/com/guiweber/estacionamento/config/SpringJpaAuditingConfig.java
+++ src/main/java/com/guiweber/estacionamento/config/SpringJpaAuditingConfig.java
@@ -2,12 +2,14 @@ package com.guiweber.estacionamento.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+@EnableJpaAuditing
 @Configuration
 public class SpringJpaAuditingConfig implements AuditorAware<String> {
 
