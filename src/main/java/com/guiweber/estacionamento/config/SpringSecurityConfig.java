74fc7f8db4ca18d25d7e2079aaaa80cdafa06f64
Guiziin227 <gh63388@gmail.com>
2025-05-06 14:59:37 -0300

    refactor: replace @EnableWebSecurity with @EnableMethodSecurity in SpringSecurityConfig

--- src/main/java/com/guiweber/estacionamento/config/SpringSecurityConfig.java
+++ src/main/java/com/guiweber/estacionamento/config/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,7 +27,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * @version 1.0
  */
 @EnableWebMvc //habilita o Spring MVC
-@EnableWebSecurity //habilita a segurança do Spring
+@EnableMethodSecurity //habilita a segurança do Spring
 @Configuration //declara que esta classe é uma configuração do Spring
 public class SpringSecurityConfig {
 
