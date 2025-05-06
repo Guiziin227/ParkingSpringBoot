0133907fc504ec63487973cd53658088821a227e
Guiziin227 <gh63388@gmail.com>
2025-05-06 15:21:20 -0300

    feat: add custom JWT authentication entry point to handle unauthorized access

--- src/main/java/com/guiweber/estacionamento/config/SpringSecurityConfig.java
+++ src/main/java/com/guiweber/estacionamento/config/SpringSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.guiweber.estacionamento.config;
 
+import com.guiweber.estacionamento.jwt.JwtAuthenticationEntryPoint;
 import com.guiweber.estacionamento.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -13,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -48,7 +51,9 @@ public class SpringSecurityConfig {
                ).addFilterBefore(
                           jwtAuthorizationFilter(), //adiciona o filtro de autorização JWT
                           UsernamePasswordAuthenticationFilter.class
-               )
+               ).exceptionHandling(
+                       ex ->
+                               ex.authenticationEntryPoint(new JwtAuthenticationEntryPoint()))
                .build();
     }
 
