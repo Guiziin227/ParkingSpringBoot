151f2829e0e0c3108ea354c64723688026e66a07
Guiziin227 <gh63388@gmail.com>
2025-05-07 15:51:43 -0300

    feat: enhance AuthController with OpenAPI documentation for authentication endpoint

--- src/main/java/com/guiweber/estacionamento/web/controller/AuthController.java
+++ src/main/java/com/guiweber/estacionamento/web/controller/AuthController.java
@@ -3,7 +3,13 @@ package com.guiweber.estacionamento.web.controller;
 import com.guiweber.estacionamento.jwt.JwtToken;
 import com.guiweber.estacionamento.jwt.JwtUserDetailsService;
 import com.guiweber.estacionamento.web.dto.UserLoginDto;
+import com.guiweber.estacionamento.web.dto.UserResponseDto;
 import com.guiweber.estacionamento.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Autenticacao", description = "Endpoints de autenticação")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -27,6 +34,20 @@ public class AuthController {
     private final JwtUserDetailsService jwtUserDetailsService;
     private final AuthenticationManager authenticationManager;
 
+    @Operation(summary = "Autenticar na API", responses = {
+            @ApiResponse(responseCode = "200", description = "Auth successfully", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponseDto.class)
+            )),
+            @ApiResponse(responseCode = "400", description = "Valids credentials", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            )),
+            @ApiResponse(responseCode = "422", description = "Invalid request data", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorMessage.class)
+            ))
+    })
     @PostMapping("/auth")
     public ResponseEntity<?> authenticate(@RequestBody @Valid UserLoginDto userLoginDto, HttpServletRequest request) {
         log.info("Processando autenticação para o usuário: {}", userLoginDto.getUsername());
