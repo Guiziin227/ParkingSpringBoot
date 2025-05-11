7e5e8ec73ae6ca5f8705a5ceee732a879ae335e1
Guiziin227 <gh63388@gmail.com>
2025-05-11 18:30:40 -0300

    fix: change CpfUniqueViolationException to extend RuntimeException for better error handling

--- src/main/java/com/guiweber/estacionamento/exception/CpfUniqueViolationException.java
+++ src/main/java/com/guiweber/estacionamento/exception/CpfUniqueViolationException.java
@@ -1,6 +1,7 @@
 package com.guiweber.estacionamento.exception;
 
-public class CpfUniqueViolationException extends Throwable {
+public class CpfUniqueViolationException extends RuntimeException {
     public CpfUniqueViolationException(String s) {
+        super(s);
     }
 }
