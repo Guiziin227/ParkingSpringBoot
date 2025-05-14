da15c1369194db7857a749d2a9de6f456acf4a21
Guiziin227 <gh63388@gmail.com>
2025-05-14 13:34:06 -0300

    feat: add CodUniqueViolationException for handling unique constraint violations

---
+++ src/main/java/com/guiweber/estacionamento/exception/CodUniqueViolationException.java
@@ -0,0 +1,7 @@
+package com.guiweber.estacionamento.exception;
+
+public class CodUniqueViolationException extends RuntimeException {
+    public CodUniqueViolationException(String s) {
+        super(s);
+    }
+}
