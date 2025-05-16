1398a0e9ef54583a41cfcbb970f6bfcd3a264c39
Guiziin227 <gh63388@gmail.com>
2025-05-16 17:17:48 -0300

    feat: enhance ClienteVaga entity with additional fields and auditing support

--- src/main/java/com/guiweber/estacionamento/entities/ClienteVaga.java
+++ src/main/java/com/guiweber/estacionamento/entities/ClienteVaga.java
@@ -1,15 +1,74 @@
 package com.guiweber.estacionamento.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Entity
+@Table(name = "clientes_tem_vagas")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteVaga implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numero_recibo",nullable = false, unique = true, length = 15)
+    private String recibo;
+
+    @Column(name = "placa", nullable = false, length = 8)
+    private String placa;
+    @Column(name = "modelo", nullable = false, length = 45)
+    private String modelo;
+    @Column(name = "marca", nullable = false, length = 45)
+    private String marca;
+    @Column(name = "cor", nullable = false, length = 45)
+    private String cor;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_vaga", nullable = false)
+    private Vaga vaga;
+
+    @Column(name = "data_entrada", nullable = false)
+    private LocalDateTime dataEntrada;
+    @Column(name = "data_saida")
+    private LocalDateTime dataSaida;
+
+    @Column(name = "valor", columnDefinition = "DECIMAL(7,2)")
+    private BigDecimal valor;
+    @Column(name = "desconto",columnDefinition = "DECIMAL(7,2)")
+    private BigDecimal desconto;
+
+
 
     @CreatedDate
     @Column(name = "data_criacao")
@@ -24,4 +83,15 @@ public class ClienteVaga implements Serializable {
     @Column(name = "modificado_por")
     private String modificadoPor;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ClienteVaga that = (ClienteVaga) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
