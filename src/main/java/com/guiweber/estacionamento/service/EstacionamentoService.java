9d6145364d7a2efb1787287aafb295372d868c94
Guiziin227 <gh63388@gmail.com>
2025-05-16 17:57:28 -0300

    feat: add checkIn method to EstacionamentoService for handling client check-ins

--- src/main/java/com/guiweber/estacionamento/service/EstacionamentoService.java
+++ src/main/java/com/guiweber/estacionamento/service/EstacionamentoService.java
@@ -1,7 +1,15 @@
 package com.guiweber.estacionamento.service;
 
+import com.guiweber.estacionamento.entities.Cliente;
+import com.guiweber.estacionamento.entities.ClienteVaga;
+import com.guiweber.estacionamento.entities.Vaga;
+import com.guiweber.estacionamento.entities.enums.StatusVaga;
+import com.guiweber.estacionamento.util.EstacionamentoUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +20,17 @@ public class EstacionamentoService {
     private final VagaService vagaService;
 
 
+    @Transactional
+    public ClienteVaga checkIn(ClienteVaga clienteVaga) {
+        Cliente cliente = clienteService.buscaPorCpf(clienteVaga.getCliente().getCpf());
+        clienteVaga.setCliente(cliente);
+
+        Vaga vaga = vagaService.buscaPorVagaLivre();
+        vaga.setStatus(StatusVaga.OCUPADA);
+        clienteVaga.setVaga(vaga);
+        clienteVaga.setDataEntrada(LocalDateTime.now());
+        clienteVaga.setRecibo(EstacionamentoUtils.gerarRecibo());
+
+        return clienteVagaService.salvar(clienteVaga);
+    }
 }
