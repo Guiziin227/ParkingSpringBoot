f8c2bb49844b3a689faa5ad5d5aff77c6468ddca
Guiziin227 <gh63388@gmail.com>
2025-05-17 20:19:24 -0300

    feat: add Builder annotation to EstacionamentoCreateDto for improved object creation

--- src/main/java/com/guiweber/estacionamento/web/dto/EstacionamentoCreateDto.java
+++ src/main/java/com/guiweber/estacionamento/web/dto/EstacionamentoCreateDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import org.hibernate.validator.constraints.br.CPF;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class EstacionamentoCreateDto {
 
     @NotBlank
