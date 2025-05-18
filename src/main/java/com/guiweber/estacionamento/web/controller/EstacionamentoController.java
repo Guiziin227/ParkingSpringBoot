88c6535c38fdd929877f91e0073c969db10c1fe3
Guiziin227 <gh63388@gmail.com>
2025-05-18 17:00:08 -0300

    feat: add check-out endpoint for vehicles with admin access and response handling

--- src/main/java/com/guiweber/estacionamento/web/controller/EstacionamentoController.java
+++ src/main/java/com/guiweber/estacionamento/web/controller/EstacionamentoController.java
@@ -95,6 +95,24 @@ public class EstacionamentoController {
         return ResponseEntity.ok(estacionamentoResponseDto);
     }
 
+    @Operation(summary = "Operação de check-out", description = "Recurso para dar saída de um veículo do estacionamento. " +
+            "Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            parameters = {@Parameter(in = PATH, name = "recibo", description = "Número do rebibo gerado pelo check-in",
+                    required = true)
+            },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso atualzado com sucesso",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = EstacionamentoResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Número do recibo inexistente ou " +
+                            "o veículo já passou pelo check-out.",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permito ao perfil de CLIENTE",
+                            content = @Content(mediaType = " application/json;charset=UTF-8",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PutMapping("/check-out/{recibo}")
     @PreAuthorize("hasAnyRole('ADMIN','CLIENTE')")
     public ResponseEntity<EstacionamentoResponseDto> checkout(@PathVariable String recibo) {
